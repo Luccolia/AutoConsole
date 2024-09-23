23c92b0cdefda147b1436cef582ba03a96cb5ef0
Luccolia <m.vivien044@gmail.com>
2024-09-23 12:21:59 +0200

    cars class

--- Program.cs
+++ Program.cs
@@ -1,23 +1,50 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Linq;
-using System.Collections.Generic;
 
 namespace AutoConsole
 {
     public class Program
     {
-        public static List<object> carlist = new List<object>();
+        public static List<Cars> carlist = new List<Cars>();
+        static Connection conn= new Connection();
 
         public static void feladat1(){
+            conn.connection.Open();
+            string sql = "SELECT * FROM `cars`";
+            MySqlCommand cmd = new MySqlCommand(sql,conn.connection);
+
+            MySqlDataReader dr = cmd.ExecuteReader();
+            dr.Read();
             
+            do
+            {
+                Cars cars = new Cars();
+                    cars.Id = dr.GetInt32(0);
+                    cars.Brand = dr.GetString(1);
+                    cars.Type = dr.GetString(2);
+                    cars.Licence = dr.GetString(3);
+                    cars.Date = dr.GetDateTime(4);
+
+                    carlist.Add(cars);
+            }
+            while (dr.Read());
+
+            dr.Close();
+            conn.connection.Close();
         }
 
         static void Main(string[] args)
         {
+            feladat1();
+            foreach (var item in carlist)
+            {
+                Console.WriteLine($"elso feladat {item.Brand}, {item.Licence}");
+            }
+           
         }
     }
 }
