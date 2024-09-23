a12ad0a97bd1afd81eec13d0618990d71379d499
Luccolia <m.vivien044@gmail.com>
2024-09-23 11:15:38 +0200

    connection is ready to use

---
+++ Connection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace AutoConsole
+{
+    internal class Connection
+    {
+        public MySqlConnection connection;
+        public string Host;
+        public string Database;
+        public string User;
+        public string Password;
+        public string ConnectionString;
+        
+        public Connection(){
+            Host = "127.0.0.1";
+            Database = "auto";
+            User = "root";
+            Password = "";
+
+            ConnectionString = "SERVER=" + Host + ";DATABASE=" + Database + ";UID=" + User + ";PASSWORD=" + Password + ";SslMode=None";
+
+        }
+    }
+}
